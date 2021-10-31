28a0b95f35673f3b68fc5d6d05fe637b97dc0d6a
Daniel Michael <itzdanielmichael@gmail.com>
2021-10-31 12:26:17 -0400

    ⚒ update  Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,32 @@
+import requests from '../utils/requests';
+
 export default function Nav() {
   return (
-    <nav>
-      <div></div>
+    <nav className="relative">
+      {/* padding on x axis (px) */}
+      {/* spacing of 10 on x axis with space-x (each item) */}
+      <div
+        className="flex px-10 sm:px-20 text-2xl
+       whitespace-nowrap space-x-10 sm:space-x-20
+       overflow-x-scroll scrollbar-hide">
+        {Object.values(requests).map(({ title }, key) => (
+          <h2
+            key={key}
+            // last h2 gets padding right of 24
+            className="last:pr-24 cursor-pointer
+            transition duration-100 transform hover:scale-125
+            hover:text-white active:text-red-500">
+            {title}
+          </h2>
+        ))}
+      </div>
+
+      <Fade />
     </nav>
   );
 }
+
+const Fade = () => (
+  // we only defined the from color, so the to color is automatically transparent
+  <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12" />
+);
